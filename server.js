527817088714e7acb733f7b029a42fb922995849
Drew Ortega <DrewSOrtega@gmail.com>
2018-05-18 13:02:20 -0700

    added basic server.js skeleton

---
+++ server.js
@@ -0,0 +1,15 @@
+var port = 8080;
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res, next){
+  res.status(200).send("It works!");
+});
+
+app.get('*', function(req, res, next){
+  res.status(404).send("Page not found.");
+})
+
+app.listen(port, function(err){
+  console.log('Server listening on port', port);
+});
