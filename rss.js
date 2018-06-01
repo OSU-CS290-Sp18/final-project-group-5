8b789f7e497dbcaa39700b8918847c70d1210acd
Hoang Nguyen <nguyeho2@oregonstate.edu>
2018-05-31 22:58:08 -0700

    Added callback function to rss parse function & simplify structure of feed object returned

--- rss.js
+++ rss.js
@@ -8,30 +8,36 @@ function parse(url, callback)
     var xmlStr = "";
 
     request(url, function (error, response, body) {
+        var feed = {};
         if (error) {
             console.log("URL Request Error: ", error);
-            return null;
+            feed = null;
             
         } else if (response.statusCode >= 400 & response.statusCode < 600) {
             console.log("Invalid URL");
-            return null;
+            feed = null;
         } else {
             parser.parseString(body, function (err, result) {
                 if (err) {
                     console.log("XML Parse Error: ", err);
-                    return null;
+                    feed = null;
                 } else {
-                    var feed = {};
                     feed.title = result.rss.channel[0].title;
-                    feed.items = result.rss.channel[0].item;
-                    return feed;
+                    feed.items = [];
+                    for (var i = 0; i < result.rss.channel[0].item.length; i++) {
+                        var elem = result.rss.channel[0].item[i];
+                        var temp = {
+                            "title": elem.title,
+                            "link": elem.link,
+                            "description": elem.description
+                        }
+                        feed.items.push(temp);
+                    }
                 }
             });
         }
+        callback(feed);
     });
-    
-
-    callback();
 }
 
 module.exports = parse;
