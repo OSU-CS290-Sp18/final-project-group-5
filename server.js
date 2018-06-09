e9aecd8b92028fbbc516df6bd53c70d10f36fd75
Drew Ortega <DrewSOrtega@gmail.com>
2018-06-09 14:32:14 -0700

    changed how express serves static files

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ var port = 8080;
 var express = require('express');
 var app = express();
 var exphbs = require('express-handlebars');
+var path = require('path');
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 var db = require('./database.js');
+  app.use(express.static(__dirname + '/public'));
 
 db.connect(function(err){
   if(!err){
-    app.use(express.static('public/css'));
-    app.use(express.static('.')); //maybe we should specify a public/js folder because we probably don't want to host all files in the root directory
-    app.use(express.static('public/html'));
+
     require("./routes")(app);
 
     app.listen(port, function(err){
