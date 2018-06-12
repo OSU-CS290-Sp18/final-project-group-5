6fd37d1335bcadf0709aad19ce073d4784a91d4f
Drew Ortega <DrewSOrtega@gmail.com>
2018-06-12 09:54:21 -0700

    added secrets file for login

---
+++ secrets.js
@@ -0,0 +1,13 @@
+module.exports = {
+  //'url' : 'localhost',
+  //'dbName' : 'rss',
+    'password' : 'aogwcqXs',
+    'url' : 'classmongo.engr.oregonstate.edu',
+    'username' : 'cs290_ortegadr',
+    'dbName' : 'cs290_ortegadr',
+}
+
+//  'password' : 'aogwcqXs'
+//  'url' : 'classmongo.engr.oregonstate.edu',
+//  'username' : 'cs290_ortegadr',
+//  'dbName' : 'cs290_ortegadr',
